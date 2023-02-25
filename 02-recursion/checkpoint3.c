c32e723d35f8cdc78a7fd33628527353fd3c3cd2
Enricco Gemha <riccogemha@gmail.com>
2023-02-25 12:46:33 -0300

    almost

--- 02-recursion/checkpoint3.c
+++ 02-recursion/checkpoint3.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 
 // return an array with all the elements plus 1
-int increase(int v[], int l, int r) {
+void increase(int vector[], int l, int r, int *vec[]) {
     // l is the first index of the v-interval (inclusive)
     // r is the last index of the v-interval (inclusive)
-
-    increase(v, l, r-1);
-    
+    if (l > r) {
+        return 0;
+    }
+    increase(vector, l, r - 1);
+    vector[r]++;
+    return vector;
 }
 
 int main(void) {
     int n;
     printf("Enter n: ");
-    scanf("%d", &n);
     int v[n];
+    scanf("%d", &n);
     
     for (int i = 0; i < n; i++) {
         printf("Enter v[%d]: ", i);
         scanf("%d", &v[i]);
     }
-    
-    int result = my_sum(v, n);
-    printf("Sum of %d elements is %d\n", n, result);
 
+    int l;
+    printf("Enter the first index of the interval: ");
+    scanf("%d", &l);
+
+    int r;
+    printf("Enter the last index of the interval: ");
+    scanf("%d", &r);
+    
+    increase(v, l, r);
+
+    // print the result on vector variable
+    for (int i = 0; i < n; i++) {
+        printf("v[%d] = %d ", i, v[i]);
+    }
+
+    
     return 0;
 }
