31188409aa14194b8d89d0fd09bcf61cb7ed9766
Enricco Gemha <riccogemha@gmail.com>
2023-02-26 08:34:15 -0300

    checkpoint 4 finished

--- 02-recursion/checkpoint3.c
+++ 02-recursion/checkpoint3.c
@@ -5,8 +5,30 @@ void increase(int vector[], int l, int r) {
     // l is the first index of the v-interval (inclusive)
     // r is the last index of the v-interval (inclusive)
     if (l > r) {
-        return 0;
+        return;
     }
     increase(vector, l, r - 1);
     vector[r]++;
+}
+
+int main(void) {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    int v[n];
+    
+    for (int i = 0; i < n; i++) {
+        printf("Enter v[%d]: ", i);
+        scanf("%d", &v[i]);
+    }
+    
+    increase(v, 0, n-1);
+    
+    for (int i = 0; i < n; i++) {
+        printf("v[%d] = %d ", i, v[i]);
+    }
+    printf("\n");
+
+    return 0;
+
 }
